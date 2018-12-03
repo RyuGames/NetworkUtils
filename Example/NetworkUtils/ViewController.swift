3f3ae136c30a9a23bfab3d1b4b078cd7ef84cc62
Wyatt Mufson <wyatt@ryucoin.com>
2018-12-03 19:57:39 +0900

    Update example

--- Example/NetworkUtils/ViewController.swift
+++ Example/NetworkUtils/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import NetworkUtils
 
 class ViewController: UIViewController {
 
+    let networkUtils = NetworkUtils.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
