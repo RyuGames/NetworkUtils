6827d01097717761c79b0b4e976b155bc8e2fa8e
Wyatt Mufson <wyatt@ryucoin.com>
2019-06-11 22:57:32 -0700

    Added contentType and spacing fixes

--- NetworkUtils/Classes/NetworkUtils.swift
+++ NetworkUtils/Classes/NetworkUtils.swift
@@ -17,23 +17,23 @@ public final class NetworkUtils: NSObject {
     private let retryErrors = [NSURLErrorCannotConnectToHost, NSURLErrorNetworkConnectionLost, NSURLErrorNotConnectedToInternet, NSURLErrorTimedOut]
     internal var testing: Bool = false
 
-    public func post(_ urlLink:String, _ params:[String:Any] = [:], _ retry:Int = 3) -> Promise<Data> {
-        return httpMethod(urlLink: urlLink, method: .POST, params: params, retry: retry)
+    public func post(_ urlLink: String, _ params: [String: Any] = [:], _ retry: Int = 3, _ contentType: String = "application/json") -> Promise<Data> {
+        return httpMethod(urlLink: urlLink, method: .POST, params: params, retry: retry, contentType: contentType)
     }
 
-    public func get(_ urlLink:String, _ params:[String:Any] = [:], _ retry:Int = 3) -> Promise<Data> {
+    public func get(_ urlLink: String, _ params: [String: Any] = [:], _ retry: Int = 3) -> Promise<Data> {
         return httpMethod(urlLink: urlLink, method: .GET, params: params, retry: retry)
     }
 
-    public func put(_ urlLink:String, _ params:[String:Any] = [:], _ retry:Int = 3) -> Promise<Data> {
-        return httpMethod(urlLink: urlLink, method: .PUT, params: params, retry: retry)
+    public func put(_ urlLink: String, _ params: [String: Any] = [:], _ retry: Int = 3, _ contentType: String = "application/json") -> Promise<Data> {
+        return httpMethod(urlLink: urlLink, method: .PUT, params: params, retry: retry, contentType: contentType)
     }
 
-    public func delete(_ urlLink:String, _ params:[String:Any] = [:], _ retry:Int = 3) -> Promise<Data> {
+    public func delete(_ urlLink: String, _ params: [String: Any] = [:], _ retry: Int = 3) -> Promise<Data> {
         return httpMethod(urlLink: urlLink, method: .DELETE, params: params, retry: retry)
     }
 
-    private func httpMethod(urlLink:String, method:httpMethodType, params:[String:Any], retry:Int) -> Promise<Data> {
+    private func httpMethod(urlLink: String, method: httpMethodType, params: [String: Any], retry: Int, contentType: String = "application/json") -> Promise<Data> {
         return Promise<Data> { fulfill, reject in
             let url = URL(string: urlLink)
             var request = URLRequest(url: url!)
@@ -61,7 +61,7 @@ public final class NetworkUtils: NSObject {
 
             request.httpMethod = method.rawValue
             let session = URLSession.shared
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue(contentType, forHTTPHeaderField: "Content-Type")
             request.addValue("application/json", forHTTPHeaderField: "Accept")
 
             session.dataTask(with: request, completionHandler: {(data, response, error) in
